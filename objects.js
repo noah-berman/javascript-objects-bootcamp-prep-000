0f9458a382b3b49a7838e92ac7d4046d1855c23b
Noah Berman <noahberm@gmail.com>
2018-05-13 21:18:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,13 +1,16 @@
 var playlist = {
-  rihanna: "bad boy"
+  Rihanna: 'Rude Boy',
+  'Wiz Khalifa': 'Black and Yellow'
 }
 
-function updatePlaylist (playlist, artistName, songTitle) {
-  playlist.artistName = songTitle
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+
   return playlist
 }
 
-function removeFromPlaylist (playlist, artistName) {
-  delete playlist.artistName
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+
   return playlist
 }
